c55907470d1edb12e98f0e1bb9f329367b80ab89
Amirmohammad Abedini <Amirmohammad.Abedini@gmail.com>
2019-06-09 19:14:41 +0430

    User data phone number validation

--- NewsSiteBackEnd/Models/UsersDto.cs
+++ NewsSiteBackEnd/Models/UsersDto.cs
@@ -14,6 +14,7 @@ namespace NewsSiteBackEnd.Models
 		public string Password { get; set; }
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
+		[Phone(ErrorMessage = "not a valid PNUMBER")]
 		public string TelNumber { get; set; }
 		[EmailAddress(ErrorMessage = "not a valid email address")]
 		public string Email { get; set; }
